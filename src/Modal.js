0e6847fec91f9c0a41199519d3e0e37e870ed609
naama-knoll <naama2745@gmail.com>
2021-12-26 14:40:10 +0200

    add project

---
+++ src/Modal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+import { useGlobal} from './context'
+
+const Modal = () => {
+  const {isModalOpen ,closeModal}=useGlobal();
+  return <div className={`${isModalOpen ? 'show-modal modal-overlay':'modal-overlay'}`}> 
+    <div className='modal-container'>
+      <h3> modal content</h3>
+      <button className='close-modal-btn' onClick={closeModal}> <FaTimes/> </button>
+    </div>
+  </div>
+}
+
+export default Modal
